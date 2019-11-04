7ff7d9cfe80082dd19155af215cb9151151ef118
spea <spea55sugaku@outlook.jp>
2019-11-05 02:31:50 +0900

    Initial commit

---
+++ src/spea55/server.java
@@ -0,0 +1,21 @@
+package spea55;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+public class server {
+
+    private static int PORT = 10000;
+
+    public static void main(String[] args) {
+        try{
+
+            ServerSocket serverSocket = new ServerSocket(PORT);
+
+            
+
+        }catch (IOException ioe){
+            System.out.println("エラーが発生しました");
+        }
+    }
+}
