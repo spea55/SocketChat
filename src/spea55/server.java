668fe6bdf49e099ef63baffac73d94aea2b07265
spea <spea55sugaku@outlook.jp>
2019-11-16 04:33:15 +0900

    initial commit

--- src/spea55/server.java
+++ src/spea55/server.java
@@ -1,6 +1,12 @@
 package spea55;
-import java.net.*;
-import java.io.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.ServerSocket;
+import java.net.InetSocketAddress;
 
 class TCP_server{
 
